87bc45d2da6e468a46504dc4803463cf642b0dfd
ckma23 <curtiskma@gmail.com>
2019-02-23 14:19:39 -0800

    completed full CRUD for the prices endpoint

--- prices/urls.py
+++ prices/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from prices import views
 
 urlpatterns = [
-    path('prices/',views.PricesList.as_view())
+    path('prices/',views.PricesList.as_view()),
+    path('prices/<int:pk>/',views.PricesDetail.as_view())
 ]
--- prices/views.py
+++ prices/views.py
@@ -1,5 +1,9 @@
+#import the model
 from prices.models import Prices
+#import the serializer
 from prices.serializers import PricesSerializer
+
+#import the rest_framework class/methods
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,5 +20,30 @@ class PricesList(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class PricesDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Prices.objects.get(pk=pk)
+        except Prices.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        price = self.get_object(pk)
+        serializer = PricesSerializer(price)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        price = self.get_object(pk)
+        serializer = PricesSerializer(price, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        price = self.get_object(pk)
+        price.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.

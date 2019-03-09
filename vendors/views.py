13a1787903748b4dce45b0f6c69bf2799ce1d8f8
ckma23 <curtiskma@gmail.com>
2019-03-09 11:21:30 -0800

    Implemented vendors CRUD endpoint. Wrapped API urls into a version -v1 <hostname>:<port>/<vx>/<endpoint>

--- vendors/views.py
+++ vendors/views.py
@@ -1,52 +1,52 @@
 from django.http import Http404
-#import the model
-from items.models import Items
-#import the serializer
-from items.serializers import ItemsSerializer
-
 #import the rest_framework class/methods
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 
-class ItemsList(APIView):
-    permission_classes = (IsAuthenticated,)
+
+#import the model
+from vendors.models import Vendors
+#import the serializer
+from vendors.serializers import VendorsSerializer
+
+
+class VendorsList(APIView):
     def get(self, request, format=None):
-        items = Items.objects.all()
-        serializers = ItemsSerializer(items,many=True)
+        vendors = Vendors.objects.all()
+        serializers = VendorsSerializer(vendors,many=True)
         return Response(serializers.data)
 
     def post(self, request, format=None):
-        serializer = ItemsSerializer(data=request.data)
+        serializer = VendorsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-class ItemsDetail(APIView):
+class VendorsDetail(APIView):
     def get_object(self, pk):
         try:
-            return Items.objects.get(pk=pk)
-        except Items.DoesNotExist:
-            raise Http404
+            return Vendors.objects.get(pk=pk)
+        except Vendors.DoesNotExist:
+            return Http404
 
     def get(self, request, pk, format=None):
-        item = self.get_object(pk)
-        serializer = ItemsSerializer(item)
+        vendor = self.get_object(pk)
+        serializer = VendorsSerializer(vendor)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        item = self.get_object(pk)
-        serializer = ItemsSerializer(item, data=request.data)
+        vendor = self.get_object(pk)
+        serializer = VendorsSerializer(vendor, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        item = self.get_object(pk)
-        item.delete()
+        vendor = self.get_object(pk)
+        vendor.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.

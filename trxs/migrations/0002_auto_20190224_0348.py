835ef75624a970f9acfbe2c732f7e2071d3c01f2
Victor Ngo <vngo@digitalpromise.org>
2019-03-09 13:30:38 -0800

    Add back migrations

---
+++ trxs/migrations/0002_auto_20190224_0348.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('trxs', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='trxs',
+            old_name='store_id',
+            new_name='store',
+        ),
+    ]

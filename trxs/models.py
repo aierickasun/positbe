4b4b0ad7600e579e3cb8c6ad1a3bb66a0b154823
ckma23 <curtiskma@gmail.com>
2019-03-16 15:35:21 -0700

    Implemented payload validation and serialization with the transaction engine

--- trxs/models.py
+++ trxs/models.py
@@ -5,10 +5,9 @@ from items.models import Items
 class Trxs(models.Model):
     tax_dollars = models.DecimalField(decimal_places=2,max_digits=12,default=00.00)
     sale_dollars = models.DecimalField(decimal_places=2,max_digits=12,default=00.00)
-    sale_total = models.DecimalField(decimal_places=2,max_digits=12,default=00.00)    
+    sale_total = models.DecimalField(decimal_places=2,max_digits=12,default=00.00)
     store = models.ForeignKey('stores.Stores',on_delete=models.CASCADE)
     sale_time = models.DateTimeField(auto_now_add=True)
-
 
 class TrxsReceipt(models.Model):
     quantity = models.PositiveIntegerField()
